1f8628a792eb0ab62bde3d6c963d022458a8c19f
Usman Ghani <33672734+usmansahi@users.noreply.github.com>
2022-05-15 15:04:32 +0200

    class work done

--- pokedex/src/CaughtPokemon.js
+++ pokedex/src/CaughtPokemon.js
@@ -1,18 +1,22 @@
 import { useState } from 'react';
 
 function CaughtPokemon() {
-  const [caught, setCaught] = useState(0);
+  const [caught, setCaught] = useState([]);
   const date = new Date().toLocaleDateString();
   function catchPokemon() {
-    setCaught(caught + 1);
+    setCaught(caught.concat('pikachu'));
   }
-
   return (
     <div>
-      <p>
-        Caught {caught} pokemon on {date}
-      </p>
       <button onClick={catchPokemon}> let's caught pokemon</button>
+      <p>
+        Caught {caught.length} pokemon on {date}
+      </p>
+      <ul>
+        {caught.map((pokemon, index) => (
+          <li key={index}>{pokemon}</li>
+        ))}
+      </ul>
     </div>
   );
 }
